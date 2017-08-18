dc27aa49993f0feff0d26a79e53736040062fd68
yunzc <yunzc@users.noreply.github.com>
2017-08-18 10:32:18 +0100

    fix analysis and frame conversion

--- src/performanceCheck.py
+++ src/performanceCheck.py
@@ -36,11 +36,16 @@ def transformSlamData(slam_pos, slam_ori, lin_diff, ang_diff):
 def findDifference(new_slam_pos, new_slam_rpy, vic_pos, vic_rpy):
 	diff_pos = [new_slam_pos[i] - vic_pos[i] for i in range(len(vic_pos))]
 	diff_rpy = [new_slam_rpy[j] - vic_rpy[j] for j in range(len(vic_rpy))]
+	print(diff_pos)
 	return diff_pos, diff_rpy
 
 def findDistance(diff_pos):
 	return math.sqrt(diff_pos[0]**2 + diff_pos[1]**2 + diff_pos[2]**2)
 
+def rads2degs(rad):
+	degrees = rad/6.28*360
+	return degrees
+	
 class PlotAndStore(object):
 	# store data and create plots
 	def __init__(self, sets_of_data, downsample_factor=30):
@@ -60,8 +65,15 @@ class PlotAndStore(object):
 			plt.ylabel(yAxisList[i])
 			plt.title(TitlesList[i])
 			plt.show()
+			data_len = len(self.data[i])
 			print(TitlesList[i])
 			print('max value: {}'.format(str(max(self.data[i]))))
+			avg = sum(self.data[i])/data_len
+			print('avg value: {}'.format(str(avg)))
+			avg_1 = sum(self.data[i][0:int(data_len/2)])/float(int((data_len/2)))
+			avg_2 = sum(self.data[i][int(data_len/2):])/float(int((data_len/2)))
+			print('first half avg value: {}'.format(str(avg_1)))
+			print('second half avg value: {}'.format(str(avg_2))) 
 
 if __name__ == '__main__':
 	vicon_pos_0 = None # xyz from vicon 
@@ -72,7 +84,7 @@ if __name__ == '__main__':
 
 	rospy.init_node('analyzer')
 	listener = tf.TransformListener()
-	rate = rospy.Rate(10.0)
+	rate = rospy.Rate(30.0)
 
 	lin_diff = None # difference in the linear position 
 	ang_diff = None # difference in the angular position 
@@ -82,7 +94,7 @@ if __name__ == '__main__':
 	while not rospy.is_shutdown():
 		try:
 			(trans_v,rot_v) = listener.lookupTransform('vicon/yun/yun', '/vicon_origin', rospy.Time(0))
-			(trans_s, rot_s) = listener.lookupTransform('camera_link', 'odom', rospy.Time(0))
+			(trans_s, rot_s) = listener.lookupTransform('camera_link', 'map', rospy.Time(0))
 			if vicon_pos_0 == None or vicon_ori_0 == None: # find the first transform for reference 
 				vicon_pos_0 = trans_v
 				vicon_ori_0 = rot_v
